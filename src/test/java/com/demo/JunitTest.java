b3867e187cd65e53644fb4e5d94912a09284f37a
heahea7 <131749616+chaerimyoo1234@users.noreply.github.com>
2023-05-31 13:51:21 +0900

    commit3

--- src/test/java/com/demo/JunitTest.java
+++ src/test/java/com/demo/JunitTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class JunitTest {
     @Test
     public void getMulticampus() {
-        System.out.println("Multicampus11122");
+        System.out.println("Multicampus111222");
     }
 }
